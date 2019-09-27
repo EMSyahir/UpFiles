940a33c36387ff4de6b0b2ab8e89892630767356
EMSyahir <55267697+EMSyahir@users.noreply.github.com>
2019-09-27 11:00:37 +0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,5 +2,5 @@
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
 echo "<h3>Ng. Satria Utomo W.P.(155610024)</h3>"
-
+echo "<h3>Muhammad Syahir(175410182)</h3>"
 ?>
